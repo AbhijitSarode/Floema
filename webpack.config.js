02d27146b4c6b5b7d0a27dfdaf634e4974251855
AbhijitSarode <arsarode@hotmail.com>
2022-07-12 20:05:27 +0530

    Webpack configured

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,128 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
-const dirApp = path.join(__dirname,'app')
-const dirAssets = path.join(__dirname,'assets')
-const dirStyles = path.join(__dirname,'styles')
+const dirApp = path.join(__dirname, 'app');
+const dirShared = path.join(__dirname, 'shared');
+const dirStyles = path.join(__dirname, 'styles');
+// const dirVideos = path.join(__dirname, 'videos'); in case I will use videos
+const dirNode = 'node_modules';
 
 console.log(dirApp, dirAssets, dirStyles);
+
+module.exports = {
+    entry: [     
+        path.join(dirApp, 'index.js'),
+        path.join(dirStyles, 'index.scss')
+    ],
+
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()]
+    },
+
+    resolve: {
+        modules: [
+            dirApp,
+            dirShared,
+            dirStyles,
+            // dirVideos, In case I will use videos
+            dirNode
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+
+        new webpack.DefinePlugin({
+            IS_DEVELOPMENT
+        }),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './shared',
+                    to: ''
+                }
+            ]
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+                plugins: [
+                    ['gifsicle', { interlaced: true }],
+                    ['jpegtran', { progressive: true }],
+                    ['optipng', { optimizationLevel: 8 }]
+                ]
+            }
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+                    },
+                    {
+                        loader: 'css-loader'
+
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+
+            {
+                test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+                loader: 'file-loader',
+                options: {
+                    name(file) {
+                        return '[hash].[ext]'
+                    }
+                }
+            },
+
+            {
+                test: /\.(jpe?g|png|gif|svg|webp)$/i,
+                use: [
+                    {
+                        loader: ImageMinimizerPlugin.loader,
+                    }
+                ]
+            },
+
+            {
+                test: /\.(glsl|frag|vert)$/,
+                loader: 'raw-loader',
+                exclude: /node_modules/
+            },
+
+            {
+                test: /\.(glsl|frag|vert)$/,
+                loader: 'glslify-loader',
+                exclude: /node_modules/
+            },
+        ]
+    }
+}
